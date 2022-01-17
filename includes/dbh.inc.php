31ae75998fd0bef4092e8aa7d792440d04207f35
Duncan Wardlaw <duncan.wardlaw@georgebrown.ca>
2022-01-17 14:45:36 -0500

    fixed db

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,10 +1,11 @@
 <?php
-$serverName = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
+$serverName = "app-d95fbec7-6866-4a2a-9310-3ea2baf61f53-do-user-10597333-0.b.db.ondigitalocean.com";
+$dbUsername = "classifieds";
+$dbPassword = 'vUi4i3qOT62DqScR';
 $dbName = "classifieds";
+$port = 25060;
 
-$db_con = new mysqli($serverName, $dbUsername, $dbPassword, "classifieds", 8085  );
+$db_con = new mysqli($serverName, $dbUsername, $dbPassword, $dbName, $port  );
 if (!$db_con) {
     exit('Connect Error (' . mysqli_connect_errno() . ') '
            . mysqli_connect_error());
